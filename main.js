04ee4f3705fbb92c7677fb4f41208a76427efe07
suulovestocode <suhanis0904@gmail.com>
2024-07-11 13:01:40 +0530

    uu

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// main.js
 document.querySelector('#someButtonId').addEventListener('click', function() {
-    // Custom JavaScript code to execute when the button is clicked
     console.log('Button clicked!');
 });
