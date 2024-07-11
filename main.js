5407880c9286adf39b705af1cc1e16b889cceb12
suulovestocode <145927615+suulovestocode@users.noreply.github.com>
2024-07-11 12:28:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// main.js
+document.querySelector('#someButtonId').addEventListener('click', function() {
+    // Custom JavaScript code to execute when the button is clicked
+    console.log('Button clicked!');
+});
